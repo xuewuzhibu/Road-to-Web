5204f0da28cad0472c6e0f3500fd28f1feb945f5
Young <750837279@qq.com>
2022-02-11 23:33:21 +0800

    优化js-demo代码及js文档

--- docs/.vuepress/public/js-demo/js-demo.js
+++ docs/.vuepress/public/js-demo/js-demo.js
@@ -1,6 +1,9 @@
+let url = document.URL;
 function runCode(value) {
     let code = value.value;
-    let url = document.URL;
     document.write(`<script>${code}` + '<' + '/script>');
     location.reload(url);
+}
+function historyGo() {
+    window.history.go(-1);
 }
