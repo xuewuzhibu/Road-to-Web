bb9aa002415ed539bac456887b284f9114744b46
Young <750837279@qq.com>
2022-02-14 03:27:47 +0800

    优化代码和更新文档

--- docs/.vuepress/public/js-demo/js-demo.js
+++ docs/.vuepress/public/js-demo/js-demo.js
@@ -1,9 +1,29 @@
 let url = document.URL;
 function runCode(value) {
     let code = value.value;
-    document.write(`<script>${code}` + '<' + '/script>');
-    location.reload(url);
+    let a = code.split('\n');
+    console.log(a);
+    let str = a.toString();
+    if (str.indexOf('if') != -1) {
+        document.write(`<script>${code}` + '<' + '/script>');
+        location.reload(url);
+    } else {
+        let a = code.split('\n');
+        for (let i = 0; i <= a.length; i++) {
+            eval(a[i]);
+        }
+    }
 }
+function consoleRunCode(values) {
+    let code = values.value;
+    let data = code.split('\n');
+    data.pop();
+    console.info('输出结果为：');
+    for (const value of data) {
+        eval(value);
+    }
+}
+
 function historyGo() {
     window.history.go(-1);
 }
