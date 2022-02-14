ebd26f44b19c7ff4f8cf8c05dac99ea09eb77772
Young <750837279@qq.com>
2022-02-14 19:22:00 +0800

    优化代码、更新文档

--- docs/.vuepress/public/js-demo/js-demo.js
+++ docs/.vuepress/public/js-demo/js-demo.js
@@ -1,28 +1,34 @@
 let url = document.URL;
-function runCode(value) {
-    let code = value.value;
-    let a = code.split('\n');
-    console.log(a);
-    let str = a.toString();
-    if (str.indexOf('if') != -1) {
-        document.write(`<script>${code}` + '<' + '/script>');
-        location.reload(url);
+function runCode(id) {
+    let values = document.getElementById(`textarea${id}`);
+    let code = values.value;
+    if (code.indexOf('if') != -1) {
+        eval(code);
+    } else if (code.indexOf('`') != -1) {
+        eval(code);
     } else {
-        let a = code.split('\n');
-        for (let i = 0; i <= a.length; i++) {
-            eval(a[i]);
+        let data = code.split('\n');
+        data.pop();
+        for (const value of data) {
+            eval(value);
         }
     }
 }
-function consoleRunCode(values) {
+function consoleRunCode(id) {
+    let values = document.getElementById(`textarea${id}`);
     let code = values.value;
-    let data = code.split('\n');
-    data.pop();
-    console.info('输出结果为：');
-    for (const value of data) {
-        eval(value);
+    if (code.indexOf('`') != -1) {
+        console.info(`第${id}个例子输出结果为：`);
+        eval(code);
+    } else {
+        let data = code.split('\n');
+        data.pop();
+        console.info(`第${id}个例子输出结果为：`);
+        for (const value of data) {
+            eval(value);
+        }
     }
-    let log = document.getElementById('log');
+    let log = document.getElementById(`log${id}`);
     log.classList.add('log');
     log.innerHTML = '请在本页面点击F12 -> 控制台(Console)中查看输出结果！';
 }
