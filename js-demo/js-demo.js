5258fd29a7a1e45af69368605556957543bdb20f
xuewuzhibu <85844728+xuewuzhibu@users.noreply.github.com>
2022-02-23 07:12:20 +0000

    Update gh-pages to output generated at ce6c779

--- js-demo/js-demo.js
+++ js-demo/js-demo.js
@@ -1,30 +1,58 @@
 // 获取当前网页地址
 // let url = document.URL;
-function runCode(id, type) {
+function runCode(id, type, title) {
     let values = document.getElementById(`textarea${id}`);
     let code = values.value;
-    if (code.indexOf('if') != -1 || code.indexOf('`') != -1 || code.indexOf('function') != -1 || type === true) {
-        eval(code);
+    if (title === true) {
+        let preCode = document.getElementById(`code${id}`).innerHTML;
+        if (code.indexOf('if') != -1 || code.indexOf('`') != -1 || code.indexOf('function') != -1 || type === true) {
+            eval(preCode + code);
+        } else {
+            let data = code.split('\n');
+            data.pop();
+            for (const value of data) {
+                eval(preCode + value);
+            }
+        }
     } else {
-        let data = code.split('\n');
-        data.pop();
-        for (const value of data) {
-            eval(value);
+        if (code.indexOf('if') != -1 || code.indexOf('`') != -1 || code.indexOf('function') != -1 || type === true) {
+            eval(code);
+        } else {
+            let data = code.split('\n');
+            data.pop();
+            for (const value of data) {
+                eval(value);
+            }
         }
     }
 }
-function consoleRunCode(id, type) {
+function consoleRunCode(id, type, title) {
     let values = document.getElementById(`textarea${id}`);
     let code = values.value;
-    if (code.indexOf('if') != -1 || code.indexOf('`') != -1 || code.indexOf('function') != -1 || type === true) {
-        console.info(`第${id}个例子输出结果为：`);
-        eval(code);
+    if (title === true) {
+        let preCode = document.getElementById(`code${id}`).innerHTML;
+        if (code.indexOf('if') != -1 || code.indexOf('`') != -1 || code.indexOf('function') != -1 || type === true) {
+            console.info(`第${id}个例子输出结果为：`);
+            eval(preCode + code);
+        } else {
+            let data = code.split('\n');
+            data.pop();
+            console.info(`第${id}个例子输出结果为：`);
+            for (const value of data) {
+                eval(preCode + value);
+            }
+        }
     } else {
-        let data = code.split('\n');
-        data.pop();
-        console.info(`第${id}个例子输出结果为：`);
-        for (const value of data) {
-            eval(value);
+        if (code.indexOf('if') != -1 || code.indexOf('`') != -1 || code.indexOf('function') != -1 || type === true) {
+            console.info(`第${id}个例子输出结果为：`);
+            eval(code);
+        } else {
+            let data = code.split('\n');
+            data.pop();
+            console.info(`第${id}个例子输出结果为：`);
+            for (const value of data) {
+                eval(value);
+            }
         }
     }
     let log = document.getElementById(`log${id}`);
